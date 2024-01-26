f0b412620a02091bad218f02a615b25f70f6e9d3
Cristian <cristian.pozo@gmail.com>
2024-01-25 23:12:11 -0300

    Aparece alert "colaborador agregado exitosamente, pero no en la tabla

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const Formulario = ({ data, SetData, dataFilter, setDataFilter, addAlert }) => {
+const Formulario = ({ data, setData, dataFilter, setDataFilter, addAlert }) => {
   //inicializa lo que recibe
   const [datosColaborador, setDatosColaborador] = useState({
     nombre: "",
@@ -10,26 +10,31 @@ const Formulario = ({ data, SetData, dataFilter, setDataFilter, addAlert }) => {
     telefono: "",
   });
 
-  const handleInputs = (e) => {
-    const inputsId = {
-      inputNombre: "nombre",
-      inputCorreo: "correo",
-      inputEdad: "edad",
-      inputCargo: "cargo",
-      inputTelefono: "telefono",
-    };
-
-    const prop = inputsId[e.target.id];
-    if (prop) {
-      setDatosColaborador({ ...datosColaborador, [prop]: e.target.value });
+  const inputHandle = (e) => {
+    if (e.target.id === 'inputNombre') {
+      setDatosColaborador({ ...datosColaborador, nombre: e.target.value });
     }
-  };
+
+    if (e.target.id === 'inputCorreo') {
+      setDatosColaborador({ ...datosColaborador, correo: e.target.value });
+    }
+
+    if (e.target.id === 'inputEdad') {
+      setDatosColaborador({ ...datosColaborador, edad: e.target.value });
+    }
+    if (e.target.id === 'inputCargo') {
+      setDatosColaborador({ ...datosColaborador, cargo: e.target.value });
+    }
+    if (e.target.id === 'inputTelefono') {
+      setDatosColaborador({ ...datosColaborador, telefono: e.target.value });
+    }
+  }
 
   const validarDatos = (e) => {
     e.preventDefault();
     const correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const telefonoRegex = /^\d{10}$/;
-    //falta expresiones regulares para telefono y email
+    const telefonoRegex = /^\d{11}$/;
+
     if (
       datosColaborador.nombre.trim() === "" ||
       datosColaborador.correo === "" ||
@@ -39,126 +44,123 @@ const Formulario = ({ data, SetData, dataFilter, setDataFilter, addAlert }) => {
       datosColaborador.telefono === "" ||
       !telefonoRegex.test(datosColaborador.telefono)
     ) {
-      addAlert({})
-    } else if (datosColaborador.telefono.length !== 10) {
-      addAlert({})
+      addAlert({
+        texto: 'Debes completar todos los campos',
+        tipo: 'alert-danger',
+        estado: true,
+      });
+    } else if (!correoRegex.test(datosColaborador.correo)) {
+      addAlert({
+        texto: 'Correo electrónico no válido',
+        tipo: 'alert-danger',
+        estado: true,
+      });
+    } else if (!telefonoRegex.test(datosColaborador.telefono)) {
+      addAlert({
+        texto: 'El teléfono debe tener 11 dígitos',
+        tipo: 'alert-danger',
+        estado: true,
+      });
     } else {
-      const nuevoColaborador = { ...datosColaborador };
-      SetData([...data, nuevoColaborador]);
+      addAlert({
+        texto: 'Colaborador Agregado Exitosamente',
+        tipo: 'alert-success',
+        estado: true,
+      });
+      const newId = data.length
+        ? (parseInt(data[data.length - 1].id) + 1).toString()
+        : '0';
+
+      setData([...data, { ...datosColaborador, id: newId }]);
+      setDataFilter([...dataFilter, { ...datosColaborador, id: newId }]);
+
       setDatosColaborador({
-        nombre: "",
-        correo: "",
-        edad: "",
-        cargo: "",
-        telefono: "",
+        nombre: '',
+        correo: '',
+        edad: '',
+        cargo: '',
+        telefono: '',
       });
     }
-    //setData
-    //setDataFilter
-    //setDatosColaborador
-  };
-  /*
-    DatosValidar
-      ? setError({
-          error: true,
-          msg: "Debes completar todos los datos",
-          color: "warning",
-        })
-      : setError({
-          error: false,
-          msg: "Login creado exitosamente",
-          color: "success",
-        });
-    if (validarPass) {
-      setError({
-        error: false,
-        msg: "Las password no coinciden, prueba nuevamente",
-        color: "danger",
-      });
-      return;
-    }
-
-
-  };
   }
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-*/
-  return (
-    <form className="formulario" onSubmit={(e) => validarDatos(e)}>
-      <div className="form-group">
-        <br />
-        <input
-          id="inputNombre"
-          type="text"
-          name="nombre"
-          className="form-control"
-          placeholder="Nombre"
-          onChange={handleInputs}
-          value={datosColaborador.nombre}
-        />
-      </div>{" "}
-      <div className="form-group">
-        <br />
-        <input
-          id="inputCorreo"
-          type="email"
-          name="email"
-          className="form-control"
-          placeholder="micorreo@ejemplo.com"
-          onChange={handleInputs}
-          value={datosColaborador.correo}
-        />
-      </div>
-      <div className="form-group">
-        <br />
-        <input
-          id="inputEdad"
-          type="number"
-          name="edad"
-          className="form-control"
-          placeholder="Edad"
-          onChange={handleInputs}
-          value={datosColaborador.edad}
-        />
-      </div>
-      <div className="form-group">
-        <br />
-        <input
-          id="inputCargo"
-          type="text"
-          name="cargo"
-          className="form-control"
-          placeholder="Cargo del colaborador"
-          onChange={handleInputs}
-          value={datosColaborador.cargo}
-        />
-      </div>
-      <div className="form-group">
-        <br />
-        <input
-          id="inputTelefono"
-          type="number"
-          name="telefono"
-          className="form-control"
-          placeholder="Teléfono del colaborador"
-          onChange={handleInputs}
-          value={datosColaborador.telefono}
-        />
-      </div>
-      <br />
-      <button
-        type="submit"
-        className="btn btn-primary fullWidth"
-      //onClick={AddColaborador}
-      >
-        Agregar Colaborador
-      </button>
-      <br />
-    </form>
-  );
-};
+  useEffect(() => {
+    setDatosColaborador({
+      nombre: '',
+      correo: '',
+      edad: '',
+      cargo: '',
+      telefono: '',
+    });
+  }, [data]);
 
-export default Formulario;
+  return (
+    <div className="formulario col-12 col-lg-4">
+      <h3>Agregar Colaborador</h3>
+
+      <form noValidate onSubmit={(e) => validarDatos(e)}>
+        <div className="mb-3">
+          <input
+            onChange={(e) => inputHandle(e)}
+            value={datosColaborador.nombre}
+            type="text"
+            className="form-control"
+            id="inputNombre"
+            placeholder="Nombre del colaborador"
+          />
+        </div>
+
+        <div className="mb-3">
+          <input
+            onChange={(e) => inputHandle(e)}
+            value={datosColaborador.correo}
+            type="email"
+            className="form-control"
+            id="inputEmail"
+            placeholder="Email del colaborador"
+            pattern=".*"
+          />
+        </div>
+
+        <div className="mb-3">
+          <input
+            onChange={(e) => inputHandle(e)}
+            value={datosColaborador.edad}
+            type="number"
+            className="form-control"
+            id="inputEdad"
+            placeholder="Edad del colaborador"
+          />
+        </div>
+
+        <div className="mb-3">
+          <input
+            onChange={(e) => inputHandle(e)}
+            value={datosColaborador.cargo}
+            type="text"
+            className="form-control"
+            id="inputCargo"
+            placeholder="Cargo del colaborador"
+          />
+        </div>
+
+        <div className="mb-3">
+          <input
+            onChange={(e) => inputHandle(e)}
+            value={datosColaborador.telefono}
+            type="text"
+            className="form-control"
+            id="inputTelefono"
+            placeholder="Teléfono del colaborador"
+          />
+        </div>
+
+        <div className="d-grid mb-3">
+          <button type="submit" className="btn">
+            Agregar Colaborador
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
