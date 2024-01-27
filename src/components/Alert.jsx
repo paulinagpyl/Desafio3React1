a8dd696faa352b9443501f54d2deb0523f6f8421
Paulina <paulinagpyl@gmail.com>
2024-01-27 08:58:55 -0300

    probando alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,9 +1,9 @@
 const Alert = ({ alerta }) => {
   const { texto, tipo, estado } = alerta;
-
+  console.log(alerta)
   return (
     <div className="alert-box col-12 col-lg-4">
-      <div className={`alert ${estado ? tipo : "display-none"}`}>
+      <div className={`alert ${estado ? tipo : "display-none"}`}> 
         <span> {texto}</span>
       </div>
     </div>
